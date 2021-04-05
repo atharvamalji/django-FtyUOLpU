1a63324383f64df7f988f14138c9a33aa75c6639
Atharva <atharva.malji.2001@gmail.com>
2021-04-05 09:48:41 +0530

    second commit

--- app/static/js/mpform.js
+++ app/static/js/mpform.js
@@ -77,6 +77,10 @@
         modal.style.display = "none";
     }
 
+    function addMember(member, status, age, if_age, if_cod) {
+
+    }
+
     $(document).ready(function() {
         $("#sel_occupation").change(function() {
             if($("#sel_occupation").val() == "G" || $("#sel_occupation").val() == "P") {
@@ -113,6 +117,8 @@
 
         $("#mbr_status").change(function() {
             if($("#mbr_status").val() == "1") {
+                $("#edited_row :input[type=text]").val("");
+                $("#edited_row :input[type=number]").val("");
                 $(".td-dead").prop('disabled', true);
                 $(".td-alive").prop('disabled', false);
                 $(".td-dead").removeClass("bg-gray-200 border-gray-300");
@@ -120,6 +126,8 @@
                 $(".td-alive").addClass("bg-gray-200 border-gray-300");
             }
             if($("#mbr_status").val() == "0") {
+                $("#edited_row :input[type=text]").val("");
+                $("#edited_row :input[type=number]").val("");
                 $(".td-alive").prop('disabled', true);
                 $(".td-dead").prop('disabled', false);
                 $(".td-alive").removeClass("bg-gray-200 border-gray-300");
@@ -143,14 +151,56 @@
             $("#edited_row").addClass("hidden");
         })
 
+        var row_counter = 0;
+
         $("#add_edited_row").click(function() {
             if($("#mbr_type").val() == "def" || $("#mbr_status").val() == "def") {
                 alert("Please select appropriate fields form the dropdown.");
             }
             if($("#mbr_type").val() != "def" && $("#mbr_status").val() != "def") {
-                
+                if($("#mbr_status").val() == "1" && $("#mbr_age").val() != "" && $("#if_age").val() == "" && $("#if_cod").val() == "") {
+                    var trow = '<tr data-row-id='+row_counter+'>';
+                    trow += '<td class="p-1 px-3 border border-gray-400 w-40">'+ $("#mbr_type option:selected").text() +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400">'+ $("#mbr_status option:selected").text() +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400">'+ $("#mbr_age").val() +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400">'+ 'NA' +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400">'+ 'NA' +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400"><div class="px-1 m-auto flex space-x-2 w-max">';
+                    trow += '<button class="w-7 h-7 hover:bg-gray-300 rounded-full" id="close_edited_row"><i class="fas fa-trash"></i></button>';
+                    trow += '<button class="w-7 h-7 hover:bg-gray-300 rounded-full" id="close_edited_row"> <i class="fas fa-check"></i></button>';
+                    trow += '</div></td></tr>';
+                    $("#mbr_table tr:last").prev().after(trow);
+                    row_counter = row_counter + 1;
+                    $("#edited_row :input[type=text]").val("");
+                    $("#edited_row :input[type=number]").val("");
+                    $("#mbr_type").val("def");
+                    $("#mbr_status").val("def");
+                }
+                else if($("#mbr_status").val() == "0" && $("#mbr_age").val() == "" && $("#if_age").val() != "" && $("#if_cod").val() != "") {
+                    var trow = '<tr data-row-id='+row_counter+'>';
+                    trow += '<td class="p-1 px-3 border border-gray-400 w-40">'+ $("#mbr_type option:selected").text() +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400">'+ $("#mbr_status option:selected").text() +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400">'+ 'NA' +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400">'+ $("#if_age").val() +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400">'+ $("#if_cod").val() +'</td>';
+                    trow += '<td class="p-1 px-3 border border-gray-400"><div class="px-1 m-auto flex space-x-2 w-max">';
+                    trow += '<button class="w-7 h-7 hover:bg-gray-300 rounded-full" id="close_edited_row"><i class="fas fa-trash"></i></button>';
+                    trow += '<button class="w-7 h-7 hover:bg-gray-300 rounded-full" id="close_edited_row"> <i class="fas fa-check"></i></button>';
+                    trow += '</div></td></tr>';
+                    row_counter = row_counter + 1;
+                    $("#mbr_table tr:last").prev().after(trow);
+                    $("#edited_row :input[type=text]").val("");
+                    $("#edited_row :input[type=number]").val("");
+                    $("#mbr_type").val("def");
+                    $("#mbr_status").val("def");
+                }
+                else {
+                    alert("Please fill the appropriate fields before adding the member.")
+                }
             }
         })
+
+
 
 
     });
